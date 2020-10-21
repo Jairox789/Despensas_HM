60abd3bdd5d6cfca04bf8d1e1ff134175263f795
Jairo Ortega Vázquez <a2193620027@alumnos.uat.edu.mx>
2020-10-21 17:38:57 -0500

    Se comentó el código del Form 1

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,38 +16,38 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
-
+        //Salida del menú
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-
+        //Evento load
         private void Form1_Load(object sender, EventArgs e)
         {
             tssOpcion.Text = "Ventana Principal";
             tssFecha.Text = DateTime.Now.ToLongDateString();
            
         }
-
+        //Menú hora
         private void timer1_Tick(object sender, EventArgs e)
         {
             tssHora.Text = DateTime.Now.ToLongTimeString();
         }
-
+        //Acerca De
         private void acerdaDeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmAcerdaDe acercaD = new frmAcerdaDe();
             acercaD.MdiParent = this;
             acercaD.Show();
         }
-
+        //Entrar a solicitar
         private void solicitarToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Solicitar Iniciar = new Solicitar();
             Iniciar.MdiParent = this;
             Iniciar.Show();
         }
-
+        //Entrar a registro
         private void registrosToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Registros Iniciar = new Registros();
